01424726be2121e7926216f9ab80b037e417f3c7
Yuri Miranda Fialho <mirandafialho@gmail.com>
2024-08-03 21:06:45 -0300

    New files for Node section

---
+++ node/pastaA/pastaB/acessarModulos.js
@@ -0,0 +1,11 @@
+const moduloA = require('../../moduloA')
+console.log(moduloA.ola)
+
+const http = require('http')
+http.createServer((req, res) => {
+    res.write('Bom dia!')
+    res.end()
+}).listen(8080)
+
+const c = require('./pastaC/index')
+console.log(c.ola2)
